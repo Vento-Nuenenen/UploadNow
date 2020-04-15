0a59948a3d63ff79a971038bacedb77c49fdc98d
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-15 22:46:23 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'FrontendController@index')->name('front');
-Route::get('/{formID}', 'FrontendController@show');
-Route::post('/{formID}/send', 'FrontendController@send');
-
 Auth::routes();
 
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function (){
     Route::get('/overwatch', 'OverwatchController@index')->name('overwatch');
 
     Route::any('/forms', 'FormsController@index')->name('forms');
-    Route::get('/forms/show/{formID}', 'FormsController@show')->name('forms-show');
+    Route::get('/forms/show/{fid}', 'FormsController@show')->name('forms-show');
     Route::get('/forms/add', 'FormsController@create')->name('add-forms');
     Route::post('/forms/store', 'FormsController@store')->name('store-forms');
     Route::post('forms/import', 'FormsController@import')->name('import-forms');
-    Route::get('/forms/edit/{formID}', 'FormsController@edit')->name('edit-forms');
+    Route::get('/forms/edit/{fid}', 'FormsController@edit')->name('edit-forms');
     Route::post('/forms/update/{fid}', 'FormsController@update')->name('update-forms');
     Route::get('/forms/destroy/{fid}', 'FormsController@destroy')->name('destroy-forms');
+
+    Route::get('/entries/download/{eid}', 'FormsController@download')->name('entries-download');
 });
+
+Route::get('/', 'FrontendController@index')->name('front');
+Route::get('/{hash}', 'FrontendController@show');
+Route::post('/{hash}/send', 'FrontendController@send');
