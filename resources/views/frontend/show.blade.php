c024e8c19673d03082df448868c4164ff74358fe
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-16 20:25:12 +0200

    Update  input description

--- resources/views/frontend/show.blade.php
+++ resources/views/frontend/show.blade.php
@@ -14,7 +14,6 @@
                 <p class="card-text">
                     {{ $form->form_description }}
                 </p>
-                <br />
                 <hr />
                 <br />
                 <form method="post" action="{{ $form->form_hash }}/send" enctype="multipart/form-data">
@@ -23,22 +22,19 @@
                     <div class="form-group">
                         <label for="answerer_name">Vor & Nachname / nom et prénom / first & last name  (Optional)</label>
                         <input type="text" class="form-control" id="answerer_name" name="answerer_name" aria-describedby="nameHelp" />
-                        <small id="nameHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
                     <div class="form-group">
                         <label for="email">E-Mail / Courrier électronique / e-mail  (Optional)</label>
                         <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" />
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
                     <div class="form-group">
-                        <label for="file">Vor & Nachname / nom et prénom / first & last name  (Optional)</label>
+                        <label for="file">Datei / fichier / file  (required)</label>
                         <div class="custom-file mb-3">
                             <input type="file" accept="audio/*,video/*" class="custom-file-input form-control" id="file" name="filename" aria-describedby="fileHelp" required />
                             <label class="custom-file-label" for="file">Choose file</label>
                         </div>
-                        <small id="fileHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Send</button>
