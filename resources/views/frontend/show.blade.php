9e637a2165a5dd4163f75ff3b0a67728fd1a01f7
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-18 00:08:49 +0200

    Add captcha to frontend

--- resources/views/frontend/show.blade.php
+++ resources/views/frontend/show.blade.php
@@ -8,6 +8,13 @@
             </div>
         @endif
 
+        @if(session()->has('error'))
+            <div class="alert alert-danger">
+                {{ session()->get('error') }}
+            </div>
+        @endif
+
+
         <div class="card mb-3">
             <div class="card-body">
                 <h1 class="card-title">{{ $form->form_name }}</h1>
@@ -16,33 +23,33 @@
                 </p>
                 <hr />
                 <br />
-                <form method="post" action="{{ $form->form_hash }}/send" enctype="multipart/form-data">
+                <form id="{{ ReCaptcha::getFormId() }}" method="post" action="{{ $form->form_hash }}/send" enctype="multipart/form-data">
                     @csrf
 
                     <div class="form-group">
                         <label for="answerer_name">Vor & Nachname / nom et prénom / first & last name (Optional)</label>
-                        <input type="text" class="form-control" id="answerer_name" name="answerer_name" aria-describedby="nameHelp" />
+                        <input type="text" value="{{ old('answerer_name') }}" class="form-control" id="answerer_name" name="answerer_name" aria-describedby="nameHelp" />
                     </div>
 
                     <div class="form-group">
                         <label for="email">E-Mail / Courrier électronique / e-mail (Optional)</label>
-                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" />
+                        <input type="email" value="{{ old('email') }}" class="form-control" id="email" name="email" aria-describedby="emailHelp" />
                     </div>
 
                     <div class="form-group">
                         <label for="file">Datei / fichier / file (required)</label>
                         <div class="custom-file mb-3">
-                            <input type="file" accept="audio/*,video/*" class="custom-file-input form-control" id="file" name="filename" aria-describedby="fileHelp" required />
+                            <input type="file" accept="audio/*,video/*" value="{{ old('filename') }}" class="custom-file-input form-control" id="file" name="filename" aria-describedby="fileHelp" required />
                             <label class="custom-file-label" for="file">Choose file</label>
                         </div>
                         <small id="nameHelp" class="form-text text-muted">Maximum file size is 100 Megabyte!</small>
                     </div>
 
-                    {!! ReCaptcha::htmlFormSnippet() !!}
-
                     <br />
 
-                    <button type="submit" class="btn btn-primary">Send</button>
+                    <div>
+                        {!! htmlFormButton("Send", ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
+                    </div>
                 </form>
             </div>
         </div>
