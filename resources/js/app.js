b4e6df894e5177d7889957a46b86997ce020e67f
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-19 13:50:54 +0200

    Check for filesize

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,12 @@ $(".custom-file-input").on("change", function() {
     var fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
+
+var uploadField = document.getElementById("file");
+
+uploadField.onchange = function() {
+    if(this.files[0].size > 307200){
+        alert("File is too big! Max file size is 200 Megabyte!");
+        $(".custom-file-label").removeClass("selected").html("Choose file");
+    };
+};
