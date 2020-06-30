9b6625226e15a6fa7319a88bcb1265d5857282e7
Laravel Shift <shift@laravelshift.com>
2020-06-30 16:13:29 +0000

    Shift options array to fluent route methods

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['register' => false]);
 
-Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/overwatch', 'OverwatchController@index')->name('overwatch');
 
     Route::any('/forms', 'FormsController@index')->name('forms');
